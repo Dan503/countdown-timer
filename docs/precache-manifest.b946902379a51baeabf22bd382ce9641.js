a88dcffac8bdb6258e76cd6f9d8b352ce8490d48
Daniel Tonon <daniel.tonon.503@gmail.com>
2019-07-24 01:20:17 +1000

    Updating the build site

---
+++ docs/precache-manifest.b946902379a51baeabf22bd382ce9641.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d511f4261b20ca599a1d",
+    "url": "/Atomic-Categorizer/js/chunk-vendors-legacy.671c7262.js"
+  },
+  {
+    "revision": "68132d57c1258c22e91d",
+    "url": "/Atomic-Categorizer/js/app-legacy.933dc48e.js"
+  },
+  {
+    "revision": "d03c21c9cf0cbe0a22a5d426ccdb5806",
+    "url": "/Atomic-Categorizer/index.html"
+  },
+  {
+    "revision": "68132d57c1258c22e91d",
+    "url": "/Atomic-Categorizer/css/app.61918d26.css"
+  }
+];
